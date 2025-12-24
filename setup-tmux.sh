13fb18113aee296f44bbbb7e877a4ac12bf81943
Yegeun Ji <ukm0613@gmail.com>
2025-12-24 13:37:44 +0000

    change setup for tmux

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -19,4 +19,12 @@ else
     echo "✓ TPM already installed"
 fi
 
-echo "✓ Tmux setup complete! Press Ctrl-Space + I to install plugins"
+echo "Installing tmux plugins..."
+[ ! -d ~/.tmux/plugins/tmux-cpu ] && git clone https://github.com/tmux-plugins/tmux-cpu ~/.tmux/plugins/tmux-cpu && echo "✓ Installed tmux-cpu"
+[ ! -d ~/.tmux/plugins/tmux-battery ] && git clone https://github.com/tmux-plugins/tmux-battery ~/.tmux/plugins/tmux-battery && echo "✓ Installed tmux-battery"
+[ ! -d ~/.tmux/plugins/tmux ] && git clone https://github.com/catppuccin/tmux ~/.tmux/plugins/tmux && echo "✓ Installed catppuccin"
+[ ! -d ~/.tmux/plugins/vim-tmux-navigator ] && git clone https://github.com/christoomey/vim-tmux-navigator ~/.tmux/plugins/vim-tmux-navigator && echo "✓ Installed vim-tmux-navigator"
+[ ! -d ~/.tmux/plugins/tmux-sensible ] && git clone https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible && echo "✓ Installed tmux-sensible"
+[ ! -d ~/.tmux/plugins/tmuxifier ] && git clone https://github.com/jimeh/tmuxifier ~/.tmux/plugins/tmuxifier && echo "✓ Installed tmuxifier"
+
+echo "✓ Tmux setup complete! All plugins installed. Start tmux to see the new status bar!"
